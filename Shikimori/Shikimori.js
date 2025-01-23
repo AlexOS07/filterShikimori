6d2f85c8a1a104e5bcba83925acaf99e56403514
AlexOS07 <nealex.game@gmail.com>
2025-01-24 00:36:42 +0300

    Update Shikimori.js

--- Shikimori/Shikimori.js
+++ Shikimori/Shikimori.js
@@ -750,7 +750,7 @@
         var year = now.getFullYear();
         var seasons = ['winter', 'spring', 'summer', 'fall'];
         var seasonIndex = (month + 1) % 12 === 0 ? 0 : Math.floor((month + 1) / 3); // Определение индекса сезона
-        return "".concat(seasons[seasonIndex - 1], "_").concat(month === 11 ? year + 1 : year);
+        return "".concat(seasons[seasonIndex], "_e").concat(month === 11 ? year + 1 : year);
       }
       function generateDynamicSeasons() {
         var now = new Date();
